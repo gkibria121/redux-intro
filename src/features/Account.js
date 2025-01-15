a5bbd665c18c08ec9fbd731c1fe48df3cdd84f9b
Gkibria <gkibria121@gmail.com>
2025-01-16 00:10:12 +0600

    add RTK

--- src/features/Account.js
+++ src/features/Account.js
@@ -1,55 +1,85 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
 
-const AccountReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
-    case "account/withdraw":
-      if (state.balance < action.payload) return state;
-      return { ...state, balance: state.balance - action.payload };
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: action.payload.amount,
-        balance: state.balance + action.payload.amount,
-        loanPurpose: action.payload.purpose,
-      };
-    case "account/payLoan":
-      if (state.balance < state.loan) return state;
-      return {
-        ...state,
-        loan: 0,
-        balance: state.balance - state.loan,
-        loanPurpose: "",
-      };
-    default:
-      return state;
-  }
-};
+// const AccountReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "account/deposit":
+//       return { ...state, balance: state.balance + action.payload };
+//     case "account/withdraw":
+//       if (state.balance < action.payload) return state;
+//       return { ...state, balance: state.balance - action.payload };
+//     case "account/requestLoan":
+//       if (state.loan > 0) return state;
+//       return {
+//         ...state,
+//         loan: action.payload.amount,
+//         balance: state.balance + action.payload.amount,
+//         loanPurpose: action.payload.purpose,
+//       };
+//     case "account/payLoan":
+//       if (state.balance < state.loan) return state;
+//       return {
+//         ...state,
+//         loan: 0,
+//         balance: state.balance - state.loan,
+//         loanPurpose: "",
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+const AccountReducer = createSlice({
+  name: "account",
+  initialState: initialState,
+
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      prepare(amount, purpose) {
+        return { payload: { amount, purpose } };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
+        state.loan = action.payload.amount;
+        state.balance = state.balance + action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+      },
+    },
+    payLoan(state) {
+      if (state.balance < state.loan) return;
+      state.loan = 0;
+      state.balance = state.balance - state.loan;
+      state.loanPurpose = "";
+    },
+  },
+});
+
 const deposit = (amount, currency) => {
-  if (currency === "USD") return { type: "account/deposit", payload: amount };
+  if (String(currency).toLocaleLowerCase() === "usd") {
+    return { type: "account/deposit", payload: amount };
+  }
+
   return async (dispatch) => {
     const response = await fetch(
       `https://api.frankfurter.dev/v1/latest?base=USD&symbols=${currency}`
     );
     if (!response.ok) return;
     const { rates } = await response.json();
-    dispatch({ type: "account/deposit", payload: amount * rates[currency] });
+    dispatch({ type: "account/deposit", payload: amount / rates[currency] });
   };
 };
-const withdraw = (amount) => {
-  return { type: "account/withdraw", payload: amount };
-};
-const requestLoan = (amount, purpose) => {
-  return { type: "account/requestLoan", payload: { amount, purpose } };
-};
-const payLoan = () => {
-  return { type: "account/payLoan" };
-};
 
-export { AccountReducer, deposit, withdraw, requestLoan, payLoan };
+export default AccountReducer.reducer;
+export { deposit };
+export const { payLoan, requestLoan, withdraw } = AccountReducer.actions;
--- src/store.js
+++ src/store.js
@@ -1,18 +1,13 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
 import { CustomerReducer } from "./features/Customer";
-import { AccountReducer } from "./features/Account";
-import { composeWithDevTools } from "@redux-devtools/extension";
-import { thunk } from "redux-thunk";
-// Combine reducers
-const rootReducer = combineReducers({
-  account: AccountReducer,
-  customer: CustomerReducer,
-});
+import AccountReducer from "./features/Account";
+import { configureStore } from "@reduxjs/toolkit";
 
 // Create store with devtools and middleware
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk)) // Add middleware if needed
-);
+const store = configureStore({
+  reducer: {
+    account: AccountReducer,
+    customer: CustomerReducer,
+  },
+});
 
 export default store;
